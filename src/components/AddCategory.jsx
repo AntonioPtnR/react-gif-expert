b1067c3740dc52e3e0f2a191f2f1bb91293691f8
AntonioPtnR <Antonio.Paton2@alu.uclm.es>
2022-07-07 13:54:19 +0200

    First commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const AddCategory = ({onNewCategory}) => {
+    const [inputValue, setInputValue] = useState('OnePunch');
+
+    const onInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (inputValue.trim().length <= 1) return;
+        onNewCategory(inputValue.trim());
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                placeholder="Buscar gifs"
+                value = {inputValue}
+                onChange={onInputChange}
+            />
+        </form>
+    )
+}
